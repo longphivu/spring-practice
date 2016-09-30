a7a33dd7b0192ed21887a3cb020b525f40c5a8bc
Long <Long@Long-PC>
2016-09-30 00:24:34 -0400

    add embeded datasource

--- timesheet/src/test/java/timesheet/DomainAwareBase.java
+++ timesheet/src/test/java/timesheet/DomainAwareBase.java
@@ -1,9 +1,9 @@
 package timesheet;
 
-import org.hsqldb.Server;
-import org.junit.AfterClass;
+import java.io.File;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,35 +11,46 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
-@ContextConfiguration(locations = "{/persitence-beans.xml}")
+@ContextConfiguration(locations = {"/persistence-beans.xml"})
 public class DomainAwareBase extends AbstractJUnit4SpringContextTests {
-	private final String deleteScript = "classpath:/resources/sql/cleanup.sql";
+	String basePath = new File("").getAbsolutePath();
+	String deleteScript = new File("src/main/resources/sql/cleanup.sql")
+            .getAbsolutePath();
+	String insertScript = new File("src/main/resources/sql/dummyData.sql")
+            .getAbsolutePath();
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	private static Server server;
+//	private static Server server;
+//	
+//	@BeforeClass
+//	public static void startDB() {
+//		server = new Server();
+//        server.setDatabaseName(0, "mydb");
+//        server.setDatabasePath(0, "mem:mydb");
+//        server.setPort(9001);
+//        server.setLogWriter(null);
+//        server.setErrWriter(null);
+//        server.start();
+//	}
+//	
+//	@AfterClass
+//	public static void stopDB() {
+//		if (server != null)
+//			server.stop();
+//	}
 	
-	@BeforeClass
-	protected static void startDB() {
-		server = new Server();
-        server.setDatabaseName(0, "mydb");
-        server.setDatabasePath(0, "mem:mydb");
-        server.setPort(9001);
-        server.setLogWriter(null);
-        server.setErrWriter(null);
-        server.start();
-	}
-
 	@Before
-	protected void deleteAllDomainEntites() {
+	public void deleteAllDomainEntites() {
 		JdbcTestUtils.executeSqlScript(jdbcTemplate, new FileSystemResource(deleteScript), false);
 		//ScriptUtils.executeSqlScript(connection, new FileSystemResource(deleteScript));
+		JdbcTestUtils.executeSqlScript(jdbcTemplate, new FileSystemResource(insertScript), false);
 	}
+
 	
-	@AfterClass
-	protected static void stopDB() {
-		if (server != null)
-			server.stop();
+	@Test
+	public void testdummy() {
+		
 	}
 }
--- timesheet/src/test/java/timesheet/integration/HibernateConfigurationTest.java
+++ timesheet/src/test/java/timesheet/integration/HibernateConfigurationTest.java
@@ -4,10 +4,11 @@ import static org.junit.Assert.assertNotNull;
 
 import org.hibernate.SessionFactory;
 import org.hsqldb.Server;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
@@ -17,29 +18,10 @@ public class HibernateConfigurationTest extends AbstractJUnit4SpringContextTests
 	@Autowired
 	SessionFactory sf;
 	
-	Server server;
-	
-	@Before
-	public void setup(){
-		server = new Server();
-        server.setDatabaseName(0, "mydb");
-        server.setDatabasePath(0, "mem:mydb");
-        server.setPort(9001);
-        server.setLogWriter(null);
-        server.setErrWriter(null);
-        server.start();
-	}
-	
 	@Test
 	public void testHibernationConfiguration() {
 		// Spring IOC container instantiated and prepared sessionFactory
 		assertNotNull(sf);
 	}
 	
-	@After
-	public void stop(){
-		if (server != null) 
-			server.stop();
-	}
-
 }
